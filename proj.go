1a3aeb0b2e127b7bad03ed67ef9bdf1d76b64346
Tom Payne <twpayne@gmail.com>
2017-05-31 22:58:22 +0200

    Add T interface

--- proj.go
+++ proj.go
@@ -5,6 +5,13 @@ import (
 	"math"
 )
 
+// A T is a generic projection.
+type T interface {
+	Code() int
+	Forward(lat, lon float64) (E float64, N float64)
+	Reverse(E, N float64) (lat float64, lon float64)
+}
+
 // dms converts d degrees, m minutes, and s seconds to degrees.
 func dms(d, m, s float64) float64 {
 	return d + m/60 + s/3600
