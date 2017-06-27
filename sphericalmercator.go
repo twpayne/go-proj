6413895f46e80ba1bac24941635cd546ed898831
Tom Payne <twpayne@gmail.com>
2017-06-28 01:24:07 +0200

    Use ASCII instead of Unicode in spherical_mercator.go

--- sphericalmercator.go
+++ sphericalmercator.go
@@ -15,16 +15,16 @@ func (sm *SphericalMercator) Code() int {
 	return sm.code
 }
 
-// Forward converts latitude φ and longitude λ to easting E and northing N.
-func (sm *SphericalMercator) Forward(φ, λ float64) (E, N float64) {
-	E = sm.r * λ
-	N = sm.r * math.Log(math.Tan((φ+math.Pi/2)/2))
+// Forward converts latitude lat and longitude lon to easting E and northing N.
+func (sm *SphericalMercator) Forward(lat, lon float64) (E, N float64) {
+	E = sm.r * lon
+	N = sm.r * math.Log(math.Tan((lat+math.Pi/2)/2))
 	return
 }
 
-// Reverse converts easting E and northing N to latitude φ and longitude λ.
-func (sm *SphericalMercator) Reverse(E, N float64) (φ, λ float64) {
-	φ = 2*math.Atan(math.Exp(N/sm.r)) - math.Pi/2
-	λ = E / sm.r
+// Reverse converts easting E and northing N to latitude lat and longitude lon.
+func (sm *SphericalMercator) Reverse(E, N float64) (lat, lon float64) {
+	lat = 2*math.Atan(math.Exp(N/sm.r)) - math.Pi/2
+	lon = E / sm.r
 	return
 }
