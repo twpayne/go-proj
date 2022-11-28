4904063aa6d3d3a810122f6241db36b60520e62b
Tom Payne <twpayne@gmail.com>
2022-11-28 23:02:40 +0100

    feat: add Transformation.TransBounds

--- go-proj.c
+++ go-proj.c
@@ -2,6 +2,17 @@
 
 #if PROJ_VERSION_MAJOR < 8
 const char *proj_context_errno_string(PJ_CONTEXT *ctx, int err) {
-    return proj_errno_string(err);
+  return proj_errno_string(err);
 }
+#endif
+
+#if PROJ_VERSION_MAJOR < 8 ||                                                  \
+    (PROJ_VERSION_MAJOR == 8 && PROJ_VERSION_MINOR < 2)
+int proj_trans_bounds(PJ_CONTEXT *context, PJ *P, PJ_DIRECTION direction,
+                      double xmin, double ymin, double xmax, double ymax,
+                      double *out_xmin, double *out_ymin, double *out_xmax,
+                      double *out_ymax, int densify_pts) {
+  return 1;
+}
+
 #endif
--- go-proj.h
+++ go-proj.h
@@ -7,4 +7,12 @@
 const char *proj_context_errno_string(PJ_CONTEXT *ctx, int err);
 #endif
 
+#if PROJ_VERSION_MAJOR < 8 ||                                                  \
+    (PROJ_VERSION_MAJOR == 8 && PROJ_VERSION_MINOR < 2)
+int proj_trans_bounds(PJ_CONTEXT *context, PJ *P, PJ_DIRECTION direction,
+                      double xmin, double ymin, double xmax, double ymax,
+                      double *out_xmin, double *out_ymin, double *out_xmax,
+                      double *out_ymax, int densify_pts);
+#endif
+
 #endif
