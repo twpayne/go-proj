0b813fe2b26de068cad3945dc32891ac7c57a7f2
Tom Payne <twpayne@gmail.com>
2024-04-06 11:44:54 +0200

    docs: make example even clearer

--- example_test.go
+++ example_test.go
@@ -12,8 +12,11 @@ func ExamplePJ_Forward() {
 		panic(err)
 	}
 
-	// Convert Zürich's WGS84 latitude/longitude to Web Mercator.
+	// Start with Zürich's WGS84 latitude/longitude.
 	zurich4326 := proj.NewCoord(47.374444, 8.541111, 408, 0)
+	fmt.Printf("initial: x=%.6f y=%.6f z=%.6f\n", zurich4326.X(), zurich4326.Y(), zurich4326.Z())
+
+	// Convert Zürich's WGS84 latitude/longitude to Web Mercator.
 	zurich3857, err := pj.Forward(zurich4326)
 	if err != nil {
 		panic(err)
@@ -28,6 +31,7 @@ func ExamplePJ_Forward() {
 	fmt.Printf("inverse: x=%.6f y=%.6f z=%.6f", zurich4326After.X(), zurich4326After.Y(), zurich4326After.Z())
 
 	// Output:
+	// initial: x=47.374444 y=8.541111 z=408.000000
 	// forward: x=950792.127329 y=6003408.475803 z=408.000000
 	// inverse: x=47.374444 y=8.541111 z=408.000000
 }
