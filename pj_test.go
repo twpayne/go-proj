cee8d671ebee17fd69f6cd42c551f918fc3d6a69
Tom Payne <twpayne@gmail.com>
2024-10-31 23:44:05 +0000

    chore: move Float64Slice functions into pj.go

--- pj_test.go
+++ pj_test.go
@@ -443,6 +443,47 @@ func TestPJ_TransFlatCoords(t *testing.T) {
 	}
 }
 
+func TestPJ_TransFloat64Slice(t *testing.T) {
+	for i, tc := range []struct {
+		float64Slice []float64
+		expected     []float64
+		delta        float64
+	}{
+		{
+			float64Slice: nil,
+			expected:     nil,
+		},
+		{
+			float64Slice: []float64{},
+			expected:     []float64{},
+		},
+		{
+			float64Slice: []float64{723134.1266446244, 474831.4869142064},
+			expected:     []float64{54.371652, 18.612462},
+			delta:        1e-14,
+		},
+		{
+			float64Slice: []float64{723134.1266446244, 474831.4869142064, 11.1},
+			expected:     []float64{54.371652, 18.612462, 11.1},
+			delta:        1e-14,
+		},
+		{
+			float64Slice: []float64{723134.1266446244, 474831.4869142064, 11.1, 1},
+			expected:     []float64{54.371652, 18.612462, 11.1, 1},
+			delta:        1e-14,
+		},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			pj, err := proj.NewCRSToCRS("EPSG:2180", "EPSG:4326", nil)
+			assert.NoError(t, err)
+			float64Slice := slices.Clone(tc.float64Slice)
+			actual, err := pj.ForwardFloat64Slice(float64Slice)
+			assert.NoError(t, err)
+			assertInDeltaFloat64Slice(t, tc.expected, actual, tc.delta)
+		})
+	}
+}
+
 func TestPJ_NormalizeForVisualizationForNorthingEastingCRS(t *testing.T) {
 	defer runtime.GC()
 
